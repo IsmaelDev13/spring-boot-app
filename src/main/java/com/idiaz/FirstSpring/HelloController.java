17f810e305514eb7ddc715419df2726106788954
Ismael Diaz <ismael-diaz8@hotmail.es>
2024-07-07 17:57:53 +0200

    [feat] post request

--- src/main/java/com/idiaz/FirstSpring/HelloController.java
+++ src/main/java/com/idiaz/FirstSpring/HelloController.java
@@ -1,14 +1,21 @@
 package com.idiaz.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-public class HelloController{
+public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World!";
-    } 
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name) {
+        return "Hello " + name + "!";
+    }
 }
